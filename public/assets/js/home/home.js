167175e414f4fdedd1c50671188b0ba24bb88b61
GXQV&1668 <yanis.mezaber@orange.com>
2024-01-21 21:43:26 +0100

    Premier commit

--- public/assets/js/home/home.js
+++ public/assets/js/home/home.js
@@ -1,48 +1,63 @@
-$(function(){
-  // The base speed per character
-	time_setting = 30;
-  // How much to 'sway' (random * this-many-milliseconds)
-	random_setting = 100;
-  // The text to use NB use \n not real life line breaks!
-	input_text = "<div class='main'>\n\t<!-- holy moly I forgot how fast I can type when I get going -->\n\t<h1>Introduction</h1>\n\t<p>Welcome to my website, all about my adventures with code</p>\n</div><!-- .main -->";
-  // Where to fill up
-	target_setting = $("#output");
-  // Launch that function!
-	type(input_text, target_setting, 0, time_setting, random_setting);
+const body = document.body;
+const sidebar = document.querySelector('.main-sidebar');
+const openSidebar = document.querySelector('#openSidebar');
+const closeSidebar = document.querySelector('#closeSidebar');
+const toggleTheme = document.querySelector('.toggle-theme');
+const light = toggleTheme.children[0];
+const dark = toggleTheme.children[1];
+const inputFields = document.querySelectorAll('.percentage p');
 
-	$("button").click(function(){
-    // When you click, remove the button
-		$(this).fadeOut('fast', function(){
-      // Remove all the text, then show the pre area again
-			target_setting.text("").show();
-      // And start again!
-			type(input_text, target_setting, 0, time_setting, random_setting);
-		});
-	});
+openSidebar.addEventListener('click', openSidebarFunction);
+closeSidebar.addEventListener('click', closeSidebarFunction);
+toggleTheme.addEventListener('click', changeTheme);
+
+function openSidebarFunction() {
+    sidebar.style.left = '0%';
+}
+
+function closeSidebarFunction() {
+    sidebar.style.left = '-100%';
+}
+
+function changeTheme() {
+    if (body.classList.contains('dark-mode')) {
+        lightMode();
+    } else if (!body.classList.contains('dark-mode')) {
+        darkMode();
+    }
+}
+
+inputFields.forEach((e, i) => {
+    let val = parseInt(e.textContent);
+    console.log(val);
+    let circle = document.getElementById(`circle${i + 1}`);
+    let r = circle.getAttribute('r');
+    let circ = Math.PI * 2 * r;
+    let counter = 0;
+    let fillValue = (circ * (100 - val)) / 100;
+    setInterval(() => {
+        if (counter === val) {
+            clearInterval();
+        } else {
+            counter += 1;
+            e.innerText = counter + '%';
+            circle.style.strokeDashoffset = fillValue;
+        }
+    }, 1000 / val);
 });
 
-function type(input, target, current, time, random){
-  // If the current count is larger than the length of the string, then for goodness sake, stop
-	if(current > input.length){
-    // Write Complete
-		console.log("Complete.");
-    // Wait a bit, then do the complete function
-		setTimeout(function(){
-			finished(input, target, current, time, random);
-		},3000);
-		
-	}
-	else{
-		console.log(current)
-    // Increment the marker
-		current += 1;
-    // fill the target with a substring, from the 0th character to the current one
-		target.text(input.substring(0,current));
-    // Wait ...
-		setTimeout(function(){
-      // do the function again, with the newly incremented marker
-			type(input, target, current, time, random);
-      // Time it the normal time, plus a random amount of sway
-		},time + Math.random()*random);
-	}
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    darkMode();
+}
+
+function darkMode() {
+    body.classList.add('dark-mode');
+    light.classList.remove('active');
+    dark.classList.add('active');
+}
+
+function lightMode() {
+    body.classList.remove('dark-mode');
+    dark.classList.remove('active');
+    light.classList.add('active');
 }
